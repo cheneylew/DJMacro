163fd1bb0c4e4b2f1c91963723b618ac57767ff7
Deju Liu <cheneylew@163.com>
2016-11-13 20:31:31 +0800

    更新

--- deploy_pod.py
+++ deploy_pod.py
@@ -1,23 +1,40 @@
 # -*- coding: utf-8 -*-
 
-import os
+import os,re
 
 if __name__ == '__main__':
-    os.system('git add *')
-    os.system('git commit -m "更新"')
-    os.system("git push origin master")
     output = os.popen('git tag')
     text=output.read()
     arr=text.split('\n')
     print(arr)
     length=len(arr)
     tag=arr[length-2]
-
     if len(tag) >= 0:
+        #生成新版本号
         versionArray=tag.split('.')
         versionArray[2]=(str)((int)(versionArray[2])+1)
         new_version=versionArray[0]+'.'+versionArray[1]+'.'+versionArray[2]
+        #查找podspec文件
+        op1=os.popen('ls *.podspec')
+        fileName=op1.read()
+        fileName=fileName.replace('\n','')
+        #读取仓库文件
+        f=open(fileName,'r')
+        content=f.read()
+        version_re=re.compile('= \'\d+\.\d+\.\d+')
+        new_content=version_re.sub('= \''+new_version,content)
+        f.close()
+        #写入版本号
+        file_object = open(fileName, 'w')
+        file_object.write(new_content)
+        file_object.close()
+        #打tag
         os.system('git tag -m "" '+new_version)
         os.system('git push --tags')
-        spec_name=os.popen('ls *.podspec')
-        print spec_name
+        #推送本地文件到远程
+        os.system('git add *')
+        os.system('git commit -m "更新"')
+        os.system("git push origin master")
+        #更新仓库
+        os.system('pod repo push Specs '+fileName+' --allow-warnings')
+        os.system('pod repo update')
